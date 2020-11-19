670bea5bb3f9d593301b4baa2971fec5cf0ca72e
hwcho0456 <hwcho0456@gmail.com>
2020-11-19 18:31:10 +0900

    modi

--- Shell01/ex01/print_groups.sh
+++ Shell01/ex01/print_groups.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 id -Gn $FT_USER | tr ' ' ',' | tr -d '\n'
-=======
-groups  | tr ' ' ',' | tr -d '\n'
->>>>>>> 6a9ab7536b81ab64f9c9ee74b4af836fb3cedb2a
--- Shell01/ex04/MAC.sh
+++ Shell01/ex04/MAC.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 ifconfig -a | tr -d '\t' | grep "ether " | sed 's/ether //' | tr -d ' '
-=======
-ifconfig -a | grep "ether"  | tr -d " " | sed "s/ether//g"
->>>>>>> 6a9ab7536b81ab64f9c9ee74b4af836fb3cedb2a
