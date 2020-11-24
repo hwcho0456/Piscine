a26a76c6c810872adcc26c25e94ca58c96b56f2e
hwcho0456 <47029861+hwcho0456@users.noreply.github.com>
2020-11-24 11:29:18 +0900

    Update ft_iterative_factorial.c

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -1,10 +1,12 @@
 int ft_iterative_factorial(int nb)
 {
+  int result;
+  
   if (nb < 0)
     return 0;
   
-  else if (nb == 0)
-    return 1;
-    
-  return (nb * ft_iterative_factorial(nb - 1));
+  result = 1;
+  while (nb >=0)
+    result *= nb;  
+  return result;
 }
